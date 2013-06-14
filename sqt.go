9ad1755c3f56ba85b923560d1a94aa6fe88c1f4c
Finn Stokes <finn.stokes@gmail.com>
2013-06-14 15:06:21 +0930

    Added SQT transformations

--- sqt.go
+++ sqt.go
@@ -7,9 +7,9 @@ import (
 // SQT represents a transformation consisting of a scaling factor in each axis,
 // a rotation and a translation.
 type SQT struct {
-	sx, sy, sz     float32
-	qx, qy, qz, qw float32
-	tx, ty, tz     float32
+	sx, sy, sz     float64
+	qx, qy, qz, qw float64
+	tx, ty, tz     float64
 }
 
 // NewSQT creates a new identity transformation.
@@ -23,30 +23,30 @@ func NewSQT() *SQT {
 
 // SetRotation sets the rotation of the SQT transformation to the
 // angle theta about the axis (x,y,z) (which must be normalised).
-func (s *SQT) SetRotation(theta, x, y, z float32) {
-	sin := float32(math.Sin(float64(theta / 2)))
+func (s *SQT) SetRotation(theta, x, y, z float64) {
+	sin := math.Sin(theta / 2)
 	s.qx = x * sin
 	s.qy = y * sin
 	s.qz = z * sin
-	s.qw = float32(math.Cos(float64(theta / 2)))
+	s.qw = math.Cos(theta / 2)
 }
 
 // SetTranslation sets the translation of the SQT transformation.
-func (s *SQT) SetTranslation(x, y, z float32) {
+func (s *SQT) SetTranslation(x, y, z float64) {
 	s.tx = x
 	s.ty = y
 	s.tz = z
 }
 
 // SetScale sets the scale of the SQT transformation in each axis.
-func (s *SQT) SetScale(x, y, z float32) {
+func (s *SQT) SetScale(x, y, z float64) {
 	s.sx = x
 	s.sy = y
 	s.sz = z
 }
 
 // qmult calculates the Grassman product of two quaternions.
-func qmult(ax, ay, az, aw, bx, by, bz, bw float32) (cx, cy, cz, cw float32) {
+func qmult(ax, ay, az, aw, bx, by, bz, bw float64) (cx, cy, cz, cw float64) {
 	cx = aw*bx + ax*bw + ay*bz - az*by
 	cy = aw*by + ay*bw + az*bx - ax*bz
 	cz = aw*bz + az*bw + ax*by - ay*bx
@@ -56,25 +56,25 @@ func qmult(ax, ay, az, aw, bx, by, bz, bw float32) (cx, cy, cz, cw float32) {
 
 // Rotate adds another rotation by the angle theta about the axis (x,y,z)
 // (which must be normalised) to the SQT transformation.
-func (s *SQT) Rotate(theta, x, y, z float32) {
-	sin := float32(math.Sin(float64(theta / 2)))
+func (s *SQT) Rotate(theta, x, y, z float64) {
+	sin := math.Sin(theta / 2)
 	qx := x * sin
 	qy := y * sin
 	qz := z * sin
-	qw := float32(math.Cos(float64(theta / 2)))
+	qw := math.Cos(theta / 2)
 	s.qx, s.qy, s.qz, s.qw = qmult(qx, qy, qz, qw, s.qx, s.qy, s.qz, s.qw)
 	//s.tx, s.ty, s.tz, _ = qmult(qx, qy, qz, qw, qmult(s.tx, s.ty, s.tz, 0, -qx, -qy, -qz, qw))
 }
 
 // Translate adds another translation to the SQT transformation.
-func (s *SQT) Translate(x, y, z float32) {
+func (s *SQT) Translate(x, y, z float64) {
 	s.tx += x
 	s.ty += y
 	s.tz += z
 }
 
 // Scale adds another scaling factor to each axis in the SQT transformation.
-func (s *SQT) Scale(x, y, z float32) {
+func (s *SQT) Scale(x, y, z float64) {
 	s.sx *= x
 	s.sy *= y
 	s.sz *= z
@@ -84,9 +84,9 @@ func (s *SQT) Scale(x, y, z float32) {
 // transformation matrix suitable for OpenGL rendering.
 func (s *SQT) Matrix() [16]float32 {
 	return [16]float32{
-		(1 - 2*s.qy*s.qy - 2*s.qz*s.qz) * s.sx, 2*s.qx*s.qy + 2*s.qz*s.qw, 2*s.qx*s.qz - 2*s.qy*s.qw, s.tx,
-		2*s.qx*s.qy - 2*s.qz*s.qw, (1 - 2*s.qx*s.qx - 2*s.qz*s.qz) * s.sy, 2*s.qy*s.qz + 2*s.qx*s.qw, s.ty,
-		2*s.qx*s.qz + 2*s.qy*s.qw, 2*s.qy*s.qz - 2*s.qx*s.qw, (1 - 2*s.qx*s.qx - 2*s.qy*s.qy) * s.sz, s.tz,
+		float32((1 - 2*s.qy*s.qy - 2*s.qz*s.qz) * s.sx), float32(2*s.qx*s.qy - 2*s.qz*s.qw), float32(2*s.qx*s.qz + 2*s.qy*s.qw), float32(s.tx),
+		float32(2*s.qx*s.qy + 2*s.qz*s.qw), float32((1 - 2*s.qx*s.qx - 2*s.qz*s.qz) * s.sy), float32(2*s.qy*s.qz - 2*s.qx*s.qw), float32(s.ty),
+		float32(2*s.qx*s.qz - 2*s.qy*s.qw), float32(2*s.qy*s.qz + 2*s.qx*s.qw), float32((1 - 2*s.qx*s.qx - 2*s.qy*s.qy) * s.sz), float32(s.tz),
 		0, 0, 0, 1,
 	}
 }
